d725f81e13d7d9feacf3b819f33ae9477928c84d
2unhi <ironhak1106@gmail.com>
2024-10-11 01:30:24 +0900

    🔧 [chore] 폴더 구조 구성

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className="items-center justify-center">
+      <h1 className="text-center">복숭아멘토</h1>
+    </div>
+  );
+}
